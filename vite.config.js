d8284a21efe438751a8c75529d152565ee48ab27
jonty <noirritchakii@gmail.com>
2025-07-16 12:18:22 +0530

    routing fixes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.JPG"],
-  base: process.env.VITE_BASE_PATH || "/noirrit.design",
+  // base: process.env.VITE_BASE_PATH || "/noirrit.design",
+  base: "/",
 });
