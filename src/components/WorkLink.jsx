1dc4b2f04ba8c3c5601b2f692f40bd5306ea1684
jonty <noirritchakii@gmail.com>
2025-07-21 15:27:31 +0530

    hover fix test

--- src/components/WorkLink.jsx
+++ src/components/WorkLink.jsx
@@ -13,7 +13,7 @@ const WorkLink = ({ showcase, title, year }) => {
       <motion.div className="relative">
         <a
           href="#"
-          className="relative p-4 w-screen max-w-[512px] transition-all duration-300 inline-flex flex-col gap-1 py-2 no-underline text-black"
+          className="relative p-4 md:w-screen md:max-w-[512px] transition-all duration-300 inline-flex flex-col gap-1 py-2 no-underline text-black"
         >
           <motion.span
             className="underline decoration-zinc-100 underline-offset-2 cursor-pointer"
@@ -33,7 +33,7 @@ const WorkLink = ({ showcase, title, year }) => {
 
         {/* adding hover on text  */}
         <motion.div
-          className="absolute left-0 top-0 w-screen h-full max-w-[512px] inset-0 group-hover:bg-zinc-100 group-hover:rounded-lg transition-all duration-300 -z-10"
+          className="absolute left-0 top-0 w-full md:w-screen h-full md:max-w-[512px] inset-0 group-hover:bg-zinc-100 group-hover:rounded-lg transition-all duration-300 -z-10"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.3 }}
