bc84db0e7beb8397d4bbaf0f377a311d56192d78
jonty <noirritchakii@gmail.com>
2025-07-31 15:07:40 +0530

    added a album scale

--- src/components/RandomSong.jsx
+++ src/components/RandomSong.jsx
@@ -111,7 +111,7 @@ export default function RandomSong() {
                 <img
                   src={song.albumImage}
                   alt="album"
-                  className="w-full h-full object-cover rounded-full"
+                  className="w-full h-full object-cover rounded-full scale-[0.95]"
                 />
                 {/* CD Overlay */}
                 <img
