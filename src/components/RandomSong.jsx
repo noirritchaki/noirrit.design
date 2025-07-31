545711e1eb702753c91be8e0c07a24f832c745ce
jonty <noirritchakii@gmail.com>
2025-07-31 15:27:23 +0530

    omg more fine tuning

--- src/components/RandomSong.jsx
+++ src/components/RandomSong.jsx
@@ -111,7 +111,7 @@ export default function RandomSong() {
                 <img
                   src={song.albumImage}
                   alt="album"
-                  className="w-full h-full object-cover rounded-full scale-[0.80]"
+                  className="w-full h-full object-cover rounded-full scale-[0.83]"
                 />
                 {/* CD Overlay */}
                 <img
