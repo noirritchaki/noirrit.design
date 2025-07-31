d8f1c713110d609847d3775bd3ab561299944e7c
jonty <noirritchakii@gmail.com>
2025-07-31 16:44:33 +0530

    removed the aspect

--- src/components/RandomSong.jsx
+++ src/components/RandomSong.jsx
@@ -99,7 +99,7 @@ export default function RandomSong() {
               </motion.h2>
 
               {/* Card */}
-              <div className="relative bg-white rounded-[16px] p-6 shadow-md w-full max-w-sm mx-auto overflow-hidden text-center aspect-[1/1.05]">
+              <div className="relative bg-white rounded-[16px] p-6 shadow-md w-full max-w-sm mx-auto overflow-hidden text-center">
                 {/* Rotating CD */}
                 <div className="flex justify-center">
                   <motion.div
